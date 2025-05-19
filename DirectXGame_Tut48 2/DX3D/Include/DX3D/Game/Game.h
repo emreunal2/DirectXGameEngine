afd52a4afd1d86679ab1e9b6ea50221706e96969
Emre <emreflaxyunal@gmail.com>
2025-05-19 02:25:46 +0100

    Threading init

--- DirectXGame_Tut48 2/DX3D/Include/DX3D/Game/Game.h
+++ DirectXGame_Tut48 2/DX3D/Include/DX3D/Game/Game.h
@@ -4,7 +4,19 @@
 #include <DX3D/Prerequisites.h>
 #include <DX3D/Math/Rect.h>
 #include <chrono>
+#include <Windows.h>
 
+//threads
+#include <thread>
+#include <atomic>
+#include <memory>
+#include <mutex>
+
+struct DataBuffer {
+	std::mutex mutex;
+	std::condition_variable cv;
+	bool dataUpdated = false;
+};
 
 class Game
 {
@@ -32,6 +44,10 @@ protected:
 private:
 	void onDisplaySize(const Rect& size);
 	void onInternalUpdate();
+
+	void runGraphicsThread();
+	void runPhysicsThread();
+	void runNetworkingThread();
 private:
 	std::unique_ptr<InputSystem> m_inputSystem;
 	std::unique_ptr<PhysicsEngine> m_physicsEngine;
@@ -40,12 +56,24 @@ private:
 	std::unique_ptr<ResourceManager> m_resourceManager;
 	std::unique_ptr<World> m_world;
 
+	std::thread m_graphicsThread;
+	std::thread m_physicsThread;
+	std::thread m_networkingThread;
+	std::atomic<bool> m_threadRunning = true;
+
 	bool m_isRunning = true;
+	std::mutex m_dataMutex;
+
 	std::chrono::system_clock::time_point m_previousTime;
 	float m_totalTime = 0.0f;
 	float m_timeScale = 1.0f;
 	float m_gravity = 9.8f;
 
+	const DWORD GRAPHICS_CORE_MASK = 0x2;
+	const DWORD NETWORKING_CORE_MASK = 0x4;
+	const DWORD PHYSICS_CORE_MASK = 0xF8;
+
+	DataBuffer m_dataBuffer;
 	friend class GraphicsEngine;
 	friend class Display;
 };
